99784d0d44d69571ed2ba6b69f9f34f6dd621e9d
Rachele Edwards <dot.com1472@gmail.com>
2020-06-17 11:36:15 -0600

    working on moving_zeroes, so far only switching the first 0 and the 3, just keeps switching it back and forth..going back to drawing board

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,8 +4,16 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
-
-    pass
+    for i in arr:
+        if i == 0:
+            print(f'IF--> i: {i}')
+            print(arr)
+            arr[i], arr[i+1] = arr[i+1], arr[i]
+            print(f'i after next: {i}')
+        else:
+            print(f'Else--> i: {i}')
+            arr[i] = arr[i]
+    return arr
 
 
 if __name__ == '__main__':
