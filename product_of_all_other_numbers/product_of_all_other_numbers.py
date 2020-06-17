fe906b3f27c3f9a24e9c7ff6d96aa9cb506a38ed
Rachele Edwards <dot.com1472@gmail.com>
2020-06-17 14:53:09 -0600

    product is pasing both tests, did printing to help me comprehend what code was doing with the multiplying

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -4,13 +4,24 @@ Returns: a List of integers
 '''
 def product_of_all_other_numbers(arr):
     # Your code here
+    newArr = [1] * len(arr)
+    product = 1
+    for i in range(len(arr)):
+        print(f'{product}, {newArr}')
+        newArr[i] *= product
+        product *= arr[i]
 
-    pass
+    product = 1
+    for i in range(len(arr) -1, -1, -1):
+        print(f'2nd Loop: {product}, {newArr}')
+        newArr[i] *= product
+        product *= arr[i]
 
+    return newArr
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [1, 2, 3, 4, 5]
+    # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
